6f53fe40243e30326690dd2ceada8cb6ca52f487
Valentin Catalin Bengea <valentinbengea@gmail.com>
2025-01-24 19:45:59 +0100

    Added quotes support for the tokenizer

---
+++ parser/create_quoted_token.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   create_quoted_token.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vbengea < vbengea@student.42madrid.com     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/24 19:42:26 by vbengea           #+#    #+#             */
+/*   Updated: 2025/01/24 19:42:38 by vbengea          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/headers.h"
+
+t_token *create_quoted_token(const char *input, int *i)
+{
+	t_token	*new_token;
+	char	*temp;
+	char	quote;
+
+	quote = input[*i];
+	new_token = create_token(TOKEN_WORD, "");
+	(*i)++;
+	while (input[*i] && input[*i] != quote)
+	{
+		temp = malloc(ft_strlen(new_token->value) + 2);
+		ft_strcpy(temp, new_token->value);
+		temp[ft_strlen(new_token->value)] = input[*i];
+		temp[ft_strlen(new_token->value) + 1] = '\0';
+		free(new_token->value);
+		new_token->value = temp;
+		(*i)++;
+	}
+	(*i)++;
+	return (new_token);
+}
