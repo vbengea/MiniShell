dd154cb5148d48f9ffb861b7dbc08ca3ccf3d595
Valentin Catalin Bengea <valentinbengea@gmail.com>
2025-01-30 18:56:41 +0100

    Update data structure with proposed changes

--- include/data_structure.h
+++ include/data_structure.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   data_structure.h                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vbengea <vbengea@student.42madrid.com>     +#+  +:+       +#+        */
+/*   By: vbengea < vbengea@student.42madrid.com     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/22 18:34:39 by vbengea           #+#    #+#             */
-/*   Updated: 2025/01/30 11:37:50 by vbengea          ###   ########.fr       */
+/*   Updated: 2025/01/30 18:55:45 by vbengea          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ typedef enum e_node_type
 {
 	NODE_CMND,
 	NODE_PIPE,
-	NODE_REDIRECT,
+	NODE_REDIRECT, // This will no longer be a type of node.
 	NODE_AND,
 	NODE_OR ,
 	NODE_GROUP
@@ -32,14 +32,25 @@ typedef enum e_redirect_type
 	REDIRECT_HEREDOC
 }	t_redirect_type;
 
+
+/* Linked list for redirects */
+typedef struct s_redirection
+{
+	t_redirect_type			type;
+	char					*file;
+	struct s_redirection	*next;
+}	t_redirection;
+
+/* Update structure.
+	The proposed updates are commented untill they are implemented */
 typedef struct s_ast_node
 {
 	t_node_type			type;
 	t_node_type			parent_type;
-	t_redirect_type		redirect_type;
+	//t_redirection		*redirs; // Linked list for redirections
+	t_redirect_type		redirect_type; // This will no longer go here
 	char				**args;
-	char				*file;
-	// int					redirect_type; // ">" (1), "<" (2), ">>" (3).
+	char				*file; // This will no longer go here
 	struct s_ast_node	*left;
 	struct s_ast_node	*right;
 	struct s_ast_node	*parent;
@@ -48,14 +59,17 @@ typedef struct s_ast_node
 
 #endif
 
-// Ejemplo de estructura si al final decidimos usar un array de hijos:
 
-// typedef struct s_ast_node
-// {
-// 	t_node_type			type;
-// 	char				**args;
-// 	char				*file;
-// 	int					redirect_type;
-// 	struct s_ast_node	**children;
-// 	int					child_count;
-// } t_ast_node;
+
+
+
+typedef struct s_ast_node
+{
+	t_node_type        type;     // CMND, PIPE, REDIRECT, AND, OR, GROUP
+	char             **args;     // Command arguments
+	t_redirection    *redirs;    // Linked list of redirections
+	struct s_ast_node *left;     // Left child (e.g., for PIPE, AND, OR)
+	struct s_ast_node *right;    // Right child (e.g., for PIPE, AND, OR)
+	struct s_ast_node *parent;   // Parent node
+	int               side;      // Left (0) or Right (1)
+} t_ast_node;
