7322cd1466815cbc4b330b0b1ac48f72e9d1a09f
juandfloresm@gmail.com <juandfloresm@gmail.com>
2025-02-04 19:30:19 +0100

    mar 04 feb 2025 19:30:19 CET

--- executor/parser.c
+++ executor/parser.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   parser.c                                            :+:      :+:    :+:   */
+/*   parser.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: juaflore <juaflore@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jflores <jflores@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/14 20:48:23 by jflores           #+#    #+#             */
-/*   Updated: 2025/01/25 16:49:17 by juaflore         ###   ########.fr       */
+/*   Updated: 2025/02/04 19:29:35 by jflores          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,13 @@ static	t_ast_node	*get_execution_node(char *context, t_mini_token level)
 {
 	t_ast_node	*ast;
 	char		*str;
-	int			i;
 
 	ast = NULL;
 	if (level == AND)
 		ast = create_structure(context, PIPE);
 	else
 	{
-		i = 0;
-		
-		if(context[0] == '(')
+		if (context[0] == '(')
 		{
 			str = ft_substr(context, 1, ff_subcontext(context) - 2);
 			ast = get_node_by_token(SUBSHELL);
@@ -38,43 +35,46 @@ static	t_ast_node	*get_execution_node(char *context, t_mini_token level)
 	return (ast);
 }
 
+static	void	inner(t_ast_node **ast, char *s, int *i, int *k, int level)
+{
+	t_ast_node	*nod;
+
+	if (is_node((s + *i), "&&"))
+		nod = get_node_by_token(AND);
+	else if (is_node((s + *i), "||"))
+		nod = get_node_by_token(OR);
+	else
+		nod = get_node_by_token(PIPE);
+	if (*ast == NULL)
+	{
+		*ast = nod;
+		(*ast)->left = get_execution_node(ft_substr(s, *k, *i - *k), level);
+	}
+	else
+	{
+		nod->left = *ast;
+		(*ast)->right = get_execution_node(ft_substr(s, *k, *i - *k), level);
+		*ast = nod;
+	}
+	*i += 2;
+	*k = *i;
+}
+
 t_ast_node	*create_structure(char *context, t_mini_token level)
 {
 	t_ast_node	*ast;
-	t_ast_node	*nod;
-	int i = 0;
-	int k = 0;
+	int			i;
+	int			k;
 
+	i = 0;
+	k = 0;
 	ast = NULL;
 	while (context[i])
 	{
 		if (ft_isquote(context[i]) || context[i] == '(')
 			i += ff_subcontext((context + i));
 		else if (is_control_character((context + i)))
-		{
-			if (is_node((context + i), "&&"))
-				nod = get_node_by_token(AND);
-			else if (is_node((context + i), "||"))
-				nod = get_node_by_token(OR);
-			else
-				nod = get_node_by_token(PIPE);
-			if (ast == NULL)
-			{
-				ast = nod;
-				ast->left = get_execution_node(ft_substr(context, k, i - k), level);
-				nod->left->side = 0;
-			}
-			else
-			{
-				nod->left = ast;
-				nod->left->side = 0;
-				ast->right = get_execution_node(ft_substr(context, k, i - k), level);
-				ast->right->side = 1;
-				ast = nod;
-			}
-			i += 2;
-			k = i;
-		}
+			inner(&ast, context, &i, &k, level);
 		else
 			i++;
 	}
@@ -91,7 +91,7 @@ t_ast_node	*create_structure(char *context, t_mini_token level)
 t_ast_node	*build_redirect_ast(char *context)
 {
 	t_ast_node	*ast;
-	
+
 	ast = create_structure(context, AND);
 	ast_printer(ast, 0);
 	return (ast);
