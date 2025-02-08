9c35d5b885e4afe4db741884dc94b825c54039a2
Valentin Catalin Bengea <valentinbengea@gmail.com>
2025-02-08 12:25:58 +0100

    Added separated main funciton to test parser

--- main.c
+++ main.c
@@ -6,13 +6,21 @@
 /*   By: vbengea < vbengea@student.42madrid.com     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/22 12:03:44 by juaflore          #+#    #+#             */
-/*   Updated: 2025/02/08 12:02:23 by vbengea          ###   ########.fr       */
+/*   Updated: 2025/02/08 12:25:25 by vbengea          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "include/headers.h"
 
-volatile sig_atomic_t g_running = 1;
+void	assign_ids(t_ast_node *node, int *id)
+{
+	if (!node)
+		return ;
+	if (node->type == NODE_CMND)
+		node->nid = ++(*id);
+	assign_ids(node->left, id);
+	assign_ids(node->right, id);
+}
 
 int main(int argc, char **argv, char **env)
 {
@@ -53,3 +61,48 @@ int main(int argc, char **argv, char **env)
 	rl_clear_history();
 	return (0);
 }
+
+// int main(int argc, char **argv, char **env)
+// {
+// 	t_ast_node	*ast;
+// 	char 		*input;
+// 	int			files[3];
+// 	t_token		*tokens;
+// 	int			id;
+
+// 	(void) argc;
+// 	(void) argv;
+// 	set_tty();
+// 	env = copy_arr_of_strs(env, 0, 0);
+// 	while (true)
+// 	{
+// 		input = readline(GREEN "minishell$ " RESET);
+// 		if (!input)
+// 		{
+// 			write(1, "exit\n", 5);
+// 			break ;
+// 		}
+// 		else if (input[0] == '\0')
+// 		{
+// 			free(input);
+// 			continue ;
+// 		}
+// 		add_history(input);
+// 		tokens = tokenize_input(input);
+// 		ast = build_ast(tokens);
+// 		assign_ids(ast, &id);
+
+// 		free(input);
+// 		if (ast)
+// 		{
+// 			files[0] = STDIN_FILENO;
+// 			files[1] = STDOUT_FILENO;
+// 			files[2] = 0;
+// 			selector(ast, &env, files);
+// 			free_redirect_ast(ast, 0);
+// 		}
+// 	}
+// 	clear_arr_of_strs(env);
+// 	rl_clear_history();
+// 	return (0);
+// }
