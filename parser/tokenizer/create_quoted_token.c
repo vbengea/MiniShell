4cb82f617a0ee3ba67080e62352420195f40ab62
Valentin Catalin Bengea <valentinbengea@gmail.com>
2025-02-09 13:00:45 +0100

    Added flag to check if a word has an ENV var

--- parser/tokenizer/create_quoted_token.c
+++ parser/tokenizer/create_quoted_token.c
@@ -6,7 +6,7 @@
 /*   By: vbengea < vbengea@student.42madrid.com     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/24 19:42:26 by vbengea           #+#    #+#             */
-/*   Updated: 2025/02/08 19:37:10 by vbengea          ###   ########.fr       */
+/*   Updated: 2025/02/09 12:45:34 by vbengea          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,8 @@ t_token	*create_quoted_token(const char *input, int *i)
 	(*i)++;
 	while (input[*i] && input[*i] != quote)
 	{
+		if (quote == '"' && input[*i] == '$' && input[*i + 1] && (ft_isalpha(input[*i + 1]) || input[*i + 1] == '?'))
+			new_token->has_env = true;
 		temp = malloc(ft_strlen(new_token->value) + 2);
 		ft_strcpy(temp, new_token->value);
 		temp[ft_strlen(new_token->value)] = input[*i];
@@ -31,6 +33,13 @@ t_token	*create_quoted_token(const char *input, int *i)
 		new_token->value = temp;
 		(*i)++;
 	}
-	(*i)++;
+	if (input[*i] == '\0')
+		new_token->type = TOKEN_INVALID;
+	else
+		(*i)++;
 	return (new_token);
 }
+
+
+// Check later this function for possible memory leaks
+// Maybe here happens an invalid free
