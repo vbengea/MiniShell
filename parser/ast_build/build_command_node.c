7b6f436d48d2e676d7491a6a4c877bbb0e99212c
Valentin Catalin Bengea <valentinbengea@gmail.com>
2025-02-10 17:29:16 +0100

    Updated export creation & update readme

--- parser/ast_build/build_command_node.c
+++ parser/ast_build/build_command_node.c
@@ -3,30 +3,54 @@
 /*                                                        :::      ::::::::   */
 /*   build_command_node.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vbengea <vbengea@student.42madrid.com>     +#+  +:+       +#+        */
+/*   By: vbengea < vbengea@student.42madrid.com     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/26 11:43:41 by vbengea           #+#    #+#             */
-/*   Updated: 2025/02/10 12:37:40 by vbengea          ###   ########.fr       */
+/*   Updated: 2025/02/10 17:23:00 by vbengea          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/headers.h"
 
+
+void	free_tab(char **tab)
+{
+	int	i;
+
+	i = 0;
+	while (tab[i])
+	{
+		free (tab[i]);
+		i++;
+	}
+	free (tab);
+}
+
 int	count_command_words(t_token *tokens)
 {
 	int		count;
 	t_token	*temp;
+	bool	prev_export;
 
 	count = 0;
+	prev_export = false;
 	temp = tokens;
 	while (temp)
 	{
 		if (temp->type == TOKEN_WORD)
+		{
 			count++;
+			if (temp->value && ft_strcmp(temp->value, "export") == 0)
+				prev_export = true;
+			else
+				prev_export = false;
+		}
 		else if (temp->type == TOKEN_ENV_VAR)
 		{
-			count++; // one for the export word
-			count += 2; // two words for var & value
+			if (!prev_export)
+				count++; // for "export"
+			count += 2; // for var & value
+			prev_export = false;
 		}
 		else if (is_redirect_token(temp->type))
 		{
@@ -38,6 +62,7 @@ int	count_command_words(t_token *tokens)
 	}
 	return (count);
 }
+
 
 char	**allocate_cmd_args(int count)
 {
@@ -80,43 +105,44 @@ int	fill_cmd_args(t_token *tokens, char **cmd_args, t_ast_node *node)
 {
 	t_token	*current;
 	int		i;
+	bool	prev_export;
 
 	i = 0;
+	prev_export = false;
 	current = tokens;
 	while (current)
 	{
 		if (current->type == TOKEN_ENV_VAR)
 			node->env_declare = true;
 		if (current->has_env == true)
-			node->has_env  = true;
-		
-		
-		if ((current->type == TOKEN_WORD || current->type == TOKEN_ENV_VAR)
-			&& current->value && current->value[0] != '\0')
+			node->has_env = true;
+
+		if (current->type == TOKEN_WORD)
 		{
-			if (current->type == TOKEN_ENV_VAR)
+			cmd_args[i] = ft_strdup(current->value);
+			if (ft_strcmp(current->value, "export") == 0)
+				prev_export = true;
+			else
+				prev_export = false;
+			i++;
+		}
+		else if (current->type == TOKEN_ENV_VAR && current->value && current->value[0] != '\0')
+		{
+			if (!prev_export)
 			{
 				cmd_args[i] = ft_strdup("export");
 				i++;
-				char **env_tokens = ft_split(current->value, '=');
-				// check NULL return from split
-				int j = 0;
-				while (env_tokens[j])
-				{
-					cmd_args[i] = ft_strdup(env_tokens[j]);
-					i++;
-					j++;
-				}
-				// free the array returned by split
 			}
-			else
+			char **env_tokens = ft_split(current->value, '=');
+			int j = 0;
+			while (env_tokens[j])
 			{
-				cmd_args[i] = ft_strdup(current->value);
-				i++;
+				cmd_args[i++] = ft_strdup(env_tokens[j]);
+				j++;
 			}
+			prev_export = false;
+			free_tab(env_tokens);
 		}
-
-
 		else if (is_redirect_token(current->type))
 		{
 			if (!handle_redirection(node, &current))
@@ -127,37 +153,6 @@ int	fill_cmd_args(t_token *tokens, char **cmd_args, t_ast_node *node)
 	cmd_args[i] = NULL;
 	return (1);
 }
-
-
-// int	fill_cmd_args(t_token *tokens, char **cmd_args, t_ast_node *node)
-// {
-// 	t_token	*current;
-// 	int		i;
-
-// 	i = 0;
-// 	current = tokens;
-// 	while (current)
-// 	{
-// 		if (current->type == TOKEN_ENV_VAR)
-// 			node->env_declare = true;
-// 		if (current->has_env == true)
-// 			node->has_env  = true;
-// 		if (current->type == TOKEN_WORD || current->type == TOKEN_ENV_VAR)
-// 		{
-// 			cmd_args[i] = ft_strdup(current->value);
-// 			i++;
-// 		}
-// 		else if (is_redirect_token(current->type))
-// 		{
-// 			if (!handle_redirection(node, &current))
-// 				return (0);
-// 		}
-// 		current = current->next;
-// 	}
-// 	cmd_args[i] = NULL;
-// 	return (1);
-// }
-
 
 t_ast_node	*build_command_node(t_token *tokens)
 {
