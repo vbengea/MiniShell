53d77dcf2a3531f044816cab0eb136b3d43e8694
juandfloresm@gmail.com <juandfloresm@gmail.com>
2025-02-11 20:48:40 +0100

    mar 11 feb 2025 20:48:40 CET

--- parser/tokenizer/create_word_token.c
+++ parser/tokenizer/create_word_token.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   create_word_token.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vbengea < vbengea@student.42madrid.com     +#+  +:+       +#+        */
+/*   By: jflores <jflores@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/24 19:07:12 by vbengea           #+#    #+#             */
-/*   Updated: 2025/02/08 19:58:47 by vbengea          ###   ########.fr       */
+/*   Updated: 2025/02/11 20:48:28 by jflores          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,8 @@ t_token	*create_word_token(const char *input, int *i)
 	len = *i - start;
 	new_token = create_token(TOKEN_WORD, ft_strndup(input + start, len));
 	if (ft_isspace(input[*i]))
+	{
 		new_token->has_space = true;
+	}
 	return (new_token);
 }
