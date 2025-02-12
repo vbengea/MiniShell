2d5fcd64e51bd3d23529f6a8517420f568d35ce5
Valentin Catalin Bengea <valentinbengea@gmail.com>
2025-02-12 19:26:59 +0100

    Updated CD to print an error with multiple args

--- builtins/cd.c
+++ builtins/cd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cd.c                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jflores <jflores@student.42.fr>            +#+  +:+       +#+        */
+/*   By: vbengea < vbengea@student.42madrid.com     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/14 20:48:23 by jflores           #+#    #+#             */
-/*   Updated: 2025/02/11 18:36:51 by jflores          ###   ########.fr       */
+/*   Updated: 2025/02/12 19:24:59 by vbengea          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,14 @@ int	cd_bi(t_ast_node *node, char ***env)
 	char	cwd[PATH_MAX];
 	char	*path;
 
+	if (node->args[2])
+		return(printf("VJ.SH: cd: too many arguments\n"), -1);
 	path = node->args[1];
 	if (path == NULL)
 		path = get_env("HOME", *env);
 	r = chdir(path);
 	if (r != 0)
-		perror("cd builtin failed");
+		perror("VJ.SH: cd");
 	else
 	{
 		if (getcwd(cwd, sizeof(cwd)) != NULL)
