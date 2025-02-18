e3a4e0537390c51b64e762c61bafb0840bac8f87
Valentin Catalin Bengea <valentinbengea@gmail.com>
2025-02-18 10:31:00 +0100

    Added escape sequence to colors

--- include/colors.h
+++ include/colors.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   colors.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jflores <jflores@student.42.fr>            +#+  +:+       +#+        */
+/*   By: vbengea <vbengea@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/23 11:22:57 by vbengea           #+#    #+#             */
-/*   Updated: 2025/02/13 20:28:53 by jflores          ###   ########.fr       */
+/*   Updated: 2025/02/18 10:29:32 by vbengea          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,15 @@
 
 # include "headers.h"
 
-# define BLACK "\033[1;30m"
-# define RED "\033[1;31m"
-# define GREEN "\033[1;32m"
-# define YELLOW "\033[1;33m"
-# define BLUE "\033[1;34m"
-# define MAGENTA "\033[1;35m"
-# define CYAN "\033[1;36m"
-# define WHITE "\033[1;37m"
+# define BLACK "\001\033[1;30m\002"
+# define RED "\001\033[1;31m\002"
+# define GREEN "\001\033[1;32m\002"
+# define YELLOW "\001\033[1;33m\002"
+# define BLUE "\001\033[1;34m\002"
+# define MAGENTA "\001\033[1;35m\002"
+# define CYAN "\001\033[1;36m\002"
+# define WHITE "\001\033[1;37m\002"
 
-# define RESET "\033[0m"
+# define RESET "\001\033[0m\002"
 
 #endif
