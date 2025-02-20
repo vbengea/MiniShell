08a73b8bd816f0fe3861bc6e623288e19d203ab2
juandfloresm@gmail.com <juandfloresm@gmail.com>
2025-02-20 10:56:42 +0100

    Builtin norms and other fixes

--- builtins/env_set.c
+++ builtins/env_set.c
@@ -6,7 +6,7 @@
 /*   By: jflores <jflores@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/14 20:48:23 by jflores           #+#    #+#             */
-/*   Updated: 2025/02/20 10:52:06 by jflores          ###   ########.fr       */
+/*   Updated: 2025/02/20 10:56:35 by jflores          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -102,7 +102,7 @@ void	set_env(t_ast_node *node, char *key, char *value, t_terminal *tty)
 	{
 		if (str)
 		{
-			printf("export: `%s': not a valid identifier", str);
+			printf("export: `%s': not a valid identifier\n", str);
 			if (str)
 				free(str);
 		}
